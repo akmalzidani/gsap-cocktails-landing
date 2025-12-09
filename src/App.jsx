d35b6f21c951e9fa140e974adf80638ab9a9343e
akmalzidani <akmal20031003@gmail.com>
2025-12-09 11:38:53 +0700

    init + setup app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import gsap, { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  return (
+    <div className="flex-center h-dvh">
+      <h1 className="text-3xl text-indigo-300">Halo GSAP</h1>
+    </div>
+  );
+}
+
+export default App;
