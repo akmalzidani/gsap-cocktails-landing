93875cf415c513bc421270b74732ba191717b545
akmalzidani <akmal20031003@gmail.com>
2025-12-18 00:48:55 +0700

    feat: add Contact component with GSAP animations and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Cocktails from "./components/Cocktails.jsx";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -19,6 +20,7 @@ const App = () => {
       <About />
       <Art />
       <Menu />
+      <Contact />
     </main>
   );
 };
